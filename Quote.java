60ad93a4d5abc8be5eac80e806bd37c9dc0cab0e
Joung MyeongHee <jmhee3411@gmail.com>
2024-12-20 12:15:35 +0900

    feat: 명언 게시판

---
+++ Quote.java
@@ -0,0 +1,32 @@
+import java.util.HashMap;
+import java.util.Map;
+import org.json.simple.JSONObject;
+
+public class Quote {
+    Map<Integer, Quote> quoteMap;
+    int id;
+    private String quote;
+    private String author;
+
+    public Quote(int id, String quote, String author) {
+        this.id = id;
+        this.quote = quote;
+        this.author = author;
+    }
+
+    public String getQuote() {
+        return this.quote;
+    }
+
+    public String getAuthor() {
+        return this.author;
+    }
+
+    public JSONObject toJson() {
+        JSONObject obj = new JSONObject();
+        obj.put("id", this.id);
+        obj.put("quote", this.quote);
+        obj.put("author", this.author);
+        return obj;
+    }
+}
